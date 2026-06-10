f5b2357852fb5e467b9ebd6612514e287439be7d
iwan <iwantelegram@gmail.com>
2026-06-11 00:49:50 +0700

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -1,8 +1,14 @@
 from pyrogram import Client, filters
+from config import OWNER_ID
 from db.mongo import partners
+
+def is_owner(user_id):
+    return user_id == OWNER_ID
 
 @Client.on_message(filters.command("pause") & filters.private)
 async def pause(client, message):
+    if not is_owner(message.from_user.id):
+        return await message.reply("❌ Kamu bukan owner bot.")
     try:
         _, channel_id, reason = message.text.split(" ", 2)
         partners.update_one({"_id": int(channel_id)}, {"$set": {"paused": True, "reason": reason}})
@@ -12,6 +18,8 @@ async def pause(client, message):
 
 @Client.on_message(filters.command("run") & filters.private)
 async def run(client, message):
+    if not is_owner(message.from_user.id):
+        return await message.reply("❌ Kamu bukan owner bot.")
     try:
         _, channel_id, reason = message.text.split(" ", 2)
         partners.update_one({"_id": int(channel_id)}, {"$set": {"paused": False, "reason": reason}})
