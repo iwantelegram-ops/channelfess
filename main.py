b361f5e31aeff2aff4d9285455ddd6eb51271365
iwan <iwantelegram@gmail.com>
2026-06-11 01:05:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-import importlib
-import os
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Load plugins
-for file in os.listdir("plugins"):
-    if file.endswith(".py"):
-        importlib.import_module(f"plugins.{file[:-3]}")
+# Gunakan plugins_dir bawaan Pyrogram
+app = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
 
 print("Bot started...")
 app.run()
