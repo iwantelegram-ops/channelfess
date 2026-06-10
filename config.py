55a9ba54fd0d7d69bc66c463971f1eb9e1eaf880
iwan <iwantelegram@gmail.com>
2026-06-11 00:30:23 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+MAIN_CHANNEL_ID = int(os.getenv("MAIN_CHANNEL_ID"))
