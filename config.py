9289f023b7a01da5dd208c94962e49386977d876
iwan <iwantelegram@gmail.com>
2026-06-11 12:34:52 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -11,6 +11,10 @@ MAIN_CHANNEL_ID       = int(os.getenv("MAIN_CHANNEL_ID", "0"))
 MAIN_CHANNEL_USERNAME = os.getenv("MAIN_CHANNEL_USERNAME", "")
 OWNER_ID              = int(os.getenv("OWNER_ID", "0"))
 BOT_USERNAME          = os.getenv("BOT_USERNAME", "")
+OWNER_USERNAME        = os.getenv("OWNER_USERNAME", "")
+OWNER_NAME            = os.getenv("OWNER_NAME", "Owner")
+BOT_NAME              = os.getenv("BOT_NAME", "FessBot")
+BOT_DESC              = os.getenv("BOT_DESC", "Auto Repost Bot")
 
 # Flood control
 FLOOD_SLEEP_THRESHOLD = 60   # detik — jika FloodWait > angka ini, skip
