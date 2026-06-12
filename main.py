d1a203d1587cb820f7b9265de69b7b2f60d56373
iwan <iwantelegram@gmail.com>
2026-06-12 07:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db.mongo import sessions_col, peers_col
 from db.mongo_storage import MongoStorage
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
